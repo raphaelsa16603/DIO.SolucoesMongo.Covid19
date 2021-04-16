0a67604c5ea1fd69f19d8730161803804d4de8e7
raphaelsa16603 <raphaelsa16603@gmail.com>
2021-04-16 19:12:23 -0300

    Controller com todas operações de CRUD, mas com problema com atualização da localização

--- MongoAPI/Api/Controllers/InfectadoController.cs
+++ MongoAPI/Api/Controllers/InfectadoController.cs
@@ -1,5 +1,7 @@
-﻿using Api.Data.Collections;
+﻿using System.Threading.Tasks;
+using Api.Data.Collections;
 using Api.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using MongoDB.Driver;
 
@@ -20,23 +22,111 @@ namespace Api.Controllers
         }
 
         [HttpPost]
-        public ActionResult SalvarInfectado([FromBody] InfectadoDto dto)
+        public async Task<IActionResult> SalvarInfectado([FromBody] InfectadoDto dto)
         {
             var infectado = new Infectado(dto.DataNascimento, dto.Sexo, 
             dto.Latitude, dto.Longitude, 
             dto.DataObito);
-
-            _infectadosCollection.InsertOne(infectado);
-            
-            return StatusCode(201, "Infectado adicionado com sucesso");
+            try
+            {
+                await _infectadosCollection.InsertOneAsync(infectado);
+                
+                return StatusCode(201, "Infectado adicionado com sucesso");
+            }
+            catch (System.Exception ex)
+            {
+                return this.StatusCode(
+                    StatusCodes.Status500InternalServerError,
+                    $"Banco de Dados Falhou : {ex.Message}");
+            }
         }
 
         [HttpGet]
-        public ActionResult ObterInfectados()
+        public async Task<IActionResult> ObterInfectados()
         {
-            var infectados = _infectadosCollection.Find(Builders<Infectado>.Filter.Empty).ToList();
+            try
+            {
+                var oInfectados = await  _infectadosCollection.FindAsync
+                                    (Builders<Infectado>.Filter.Empty);
+                var infectados = oInfectados.ToList();
             
-            return Ok(infectados);
+                return Ok(infectados);
+            }
+            catch (System.Exception ex)
+            {
+                return this.StatusCode(
+                    StatusCodes.Status500InternalServerError,
+                    $"Banco de Dados Falhou : {ex.Message}");
+            }
+        }
+
+        [HttpGet("{uid}")]
+        public async Task<IActionResult> Get(string uid)
+        {
+            var filter = Builders<Infectado>.Filter.Eq(inf => inf.uId, uid);
+            try
+            {
+                var obj = await  _infectadosCollection.FindAsync(filter);
+                var results = obj.ToList();
+                return Ok(results);    
+            }
+            catch (System.Exception ex)
+            {
+                return this.StatusCode(
+                    StatusCodes.Status500InternalServerError,
+                    $"Banco de Dados Falhou : {ex.Message}");
+            }
+        }
+
+
+        // PUT api/values/5
+        [HttpPut("{uid}")]
+        public async void Put(string uid, [FromBody] InfectadoDto dto)
+        {
+            var filter = Builders<Infectado>.Filter.Eq(inf => inf.uId, uid);
+            UpdateDefinition<Infectado> update = null;
+            if (dto.DataNascimento != null)
+                update = Builders<Infectado>.Update
+                        .Set( s => s.DataNascimento, dto.DataNascimento);
+            
+            if (dto.DataObito != null)
+                update = Builders<Infectado>.Update
+                        .Set( s => s.DataObito, dto.DataObito);
+            if (dto.Sexo != null)
+                update = Builders<Infectado>.Update
+                        .Set( s => s.Sexo, dto.Sexo);
+                        
+            // update = Builders<Infectado>.Update
+            //             .Set( s => s.Localizacao.Longitude, dto.Longitude);
+            // update = Builders<Infectado>.Update            
+            //              .Set( s => s.Localizacao.Latitude, dto.Latitude);
+            
+            try
+            {
+                if(update != null)
+                {
+                    var result = await  _infectadosCollection.UpdateOneAsync(filter, update);
+                }
+            }
+            catch (System.Exception ex) //(System.Exception ex)
+            {
+                //$"Banco de Dados Falhou : {ex.Message}"
+            }
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{uid}")]
+        public async void Delete(string uid)
+        {
+            var filter = Builders<Infectado>.Filter.Eq(inf => inf.uId, uid);
+            try
+            {
+                var result = await  _infectadosCollection.DeleteOneAsync(filter);
+            }
+            catch (System.Exception) //(System.Exception ex)
+            {
+                //$"Banco de Dados Falhou : {ex.Message}"
+            }
         }
     }
 }
