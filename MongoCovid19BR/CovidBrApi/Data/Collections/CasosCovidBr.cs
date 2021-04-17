a9f29ea1d6e7ba484db8547f8b5668a2b8561b31
raphaelsa16603 <raphaelsa16603@gmail.com>
2021-04-16 21:01:10 -0300

    Iniciando configurações do MongoDB

---
+++ MongoCovid19BR/CovidBrApi/Data/Collections/CasosCovidBr.cs
@@ -0,0 +1,38 @@
+using System;
+
+namespace CovidBrApi.Data.Collections
+{
+    public class CasosCovidBr
+    {
+        public string city { get; set; }
+        public string city_ibge_code { get; set; }
+
+        public DateTime date { get; set; }
+
+        public string epidemiological_week { get; set; }
+
+        public long estimated_population { get; set; }
+        public long estimated_population_2019 { get; set; }
+
+        public string is_last { get; set; }
+        public string is_repeated { get; set; }
+        public long city_ibglast_available_confirmede_code { get; set; }
+
+        public double last_available_confirmed_per_100k_inhabitants { get; set; }
+
+        public DateTime last_available_date { get; set; }
+
+        public double last_available_death_rate { get; set; }
+
+        public long last_available_deaths { get; set; }
+        public long order_for_place { get; set; }
+
+        public string place_type { get; set; }
+
+        public string state { get; set; }
+
+
+        public long new_confirmed { get; set; }
+        public long new_deaths { get; set; }
+    }
+}
---
+++ MongoCovid19BR/CovidBrApi/README.md
@@ -0,0 +1,7 @@
+## Projeto do API para atualiza a base de dados de Acompanhamento do COVID19 no Brasil do MongoDB Atlas
+## Covid19BR.DadosCovid
+##
+#### https://www.nuget.org/packages/mongodb.driver
+- dotnet add package MongoDB.Driver --version 2.12.2
+- dotnet add package MongoDB.Driver 
+
