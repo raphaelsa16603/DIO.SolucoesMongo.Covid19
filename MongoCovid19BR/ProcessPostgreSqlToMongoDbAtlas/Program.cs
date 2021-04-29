2fb941ced604eb218137620a986df65cdcbbbfca
raphaelsa16603 <raphaelsa16603@gmail.com>
2021-04-29 19:14:42 -0300

    Novo projeto

---
+++ MongoCovid19BR/ProcessPostgreSqlToMongoDbAtlas/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProcessPostgreSqlToMongoDbAtlas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
